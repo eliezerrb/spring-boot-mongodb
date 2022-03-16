8083e0be9b4f619eb294bf8168e150ea29f65ddd
eliezerrb <eliezerrezende@outlook.com>
2022-03-15 21:51:21 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/eliezer/coursemongo/CoursemongoApplication.java
@@ -0,0 +1,13 @@
+package com.eliezer.coursemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliezer/coursemongo/CoursemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliezer.coursemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
