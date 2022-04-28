6723775ee2e19badc7ca567e6a0fb82973e60648
eliezerrb <eliezerrezende@outlook.com>
2022-04-27 22:47:51 -0300

    Obtendo um post por id

---
+++ src/main/java/com/eliezer/coursemongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.eliezer.coursemongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eliezer.coursemongo.domain.Post;
+import com.eliezer.coursemongo.repository.PostRepository;
+import com.eliezer.coursemongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
