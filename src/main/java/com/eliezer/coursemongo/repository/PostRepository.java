b74828520263ca8e79a60c2b8699efe724c47888
eliezerrb <eliezerrezende@outlook.com>
2022-05-02 20:37:39 -0300

    Consulta simples com query methods

--- src/main/java/com/eliezer/coursemongo/repository/PostRepository.java
+++ src/main/java/com/eliezer/coursemongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.eliezer.coursemongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.eliezer.coursemongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
