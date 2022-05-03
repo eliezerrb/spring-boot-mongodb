d3382abc3e080871893919771478fb012e2f98dd
eliezerrb <eliezerrezende@outlook.com>
2022-05-02 22:05:10 -0300

    Consulta com vários critérios

--- src/main/java/com/eliezer/coursemongo/repository/PostRepository.java
+++ src/main/java/com/eliezer/coursemongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.eliezer.coursemongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,9 +12,14 @@ import com.eliezer.coursemongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	// i(Ignorar maiuscula e minuscula)
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//gte(maior ou igual)  lte(menor ou igual)
+	@Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 	
 }
